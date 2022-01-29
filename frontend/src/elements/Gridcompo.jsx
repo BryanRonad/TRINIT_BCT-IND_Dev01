7547a0aaf7e1045eecdffcfb00624dc84be2102d
Tejasjagdale <69722269+Tejasjagdale@users.noreply.github.com>
2022-01-30 00:30:13 +0530

    gird element fixed

--- frontend/src/elements/Gridcompo.jsx
+++ frontend/src/elements/Gridcompo.jsx
@@ -4,7 +4,7 @@ const Gridcompo = (props) => {
   console.log(props.properties);
   const changeColsandRows = () => {
     document
-      .querySelector(".Grid_div")
+      .getElementById(props.id)
       .setAttribute(
         "style",
         `display: grid;grid-template-rows: repeat(${props.properties.rows}, 1fr);grid-template-columns: repeat(6, 1fr);width: 100%;height: 100px;background-color: #3b97e3`
@@ -17,7 +17,7 @@ const Gridcompo = (props) => {
 
   return (
     <>
-      <div className="Grid_div">
+      <div className="Grid_div" id={props.id} onClick={props.selectTag}>
         <div style={{ width: "50px", height: "50px", background: "red" }}></div>
       </div>
     </>
