2250c8c2ea9034603f650d1dc78adaadb282943f
Tejasjagdale <69722269+Tejasjagdale@users.noreply.github.com>
2022-01-29 22:50:16 +0530

    grid compo added

--- frontend/src/components/PropertiesTab.jsx
+++ frontend/src/components/PropertiesTab.jsx
@@ -1,7 +1,7 @@
 import { Box, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
 
-const PropertiesTab = () => {
+const PropertiesTab = (props) => {
 	return <Box className="properties-tab">This is properties</Box>;
 };
 
