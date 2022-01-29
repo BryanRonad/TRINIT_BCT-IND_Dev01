1e0cea56b45aae8544f7194bcfe89739bf414e8e
Cris10Lobo <cris10lobo@gmail.com>
2022-01-30 00:29:10 +0530

    adding props to elements

--- frontend/src/elements/AddCard.jsx
+++ frontend/src/elements/AddCard.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 const AddCard = (props) => {
-  return (
-    <>
-        <div id={props.id} style={{width:"150px", minHeight: "200px",background:"orange",margin:"10px"}} onClick={props.selectTag}>
-        
-        </div>
-    </>
-  );
+	return (
+		<>
+			<div
+				id={props.id}
+				style={{
+					width: props.properties.width,
+					minHeight: props.properties.height,
+					background: props.properties.color,
+					margin: props.properties.margin,
+					padding: props.properties.padding,
+					fontSize: props.properties.fontsize,
+					color: props.properties.fontcolor,
+					borderRadius: "15px",
+				}}
+				onClick={props.selectTag}
+			>
+				{props.children}
+			</div>
+		</>
+	);
 };
 
 export default AddCard;
--- frontend/src/elements/Gridcompo.jsx
+++ frontend/src/elements/Gridcompo.jsx
@@ -1,27 +1,27 @@
 import { Button } from "@chakra-ui/react";
 import React, { useEffect } from "react";
 const Gridcompo = (props) => {
-  console.log(props.properties);
-  const changeColsandRows = () => {
-    document
-      .querySelector(".Grid_div")
-      .setAttribute(
-        "style",
-        `display: grid;grid-template-rows: repeat(${props.properties.rows}, 1fr);grid-template-columns: repeat(6, 1fr);width: 100%;height: 100px;background-color: #3b97e3`
-      );
-  };
+	console.log(props.properties);
+	const changeColsandRows = () => {
+		document
+			.querySelector(".Grid_div")
+			.setAttribute(
+				"style",
+				`display: grid;grid-template-rows: repeat(${props.properties.rows}, 1fr);grid-template-columns: repeat(${props.properties.column}, 1fr);width: 100%;height: 100px;background-color: #3b97e3`
+			);
+	};
 
-  useEffect(() => {
-    changeColsandRows();
-  }, []);
+	useEffect(() => {
+		changeColsandRows();
+	}, []);
 
-  return (
-    <>
-      <div className="Grid_div">
-        <div style={{ width: "50px", height: "50px", background: "red" }}></div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="Grid_div">
+				<div style={{ width: "50px", height: "50px", background: "red" }}></div>
+			</div>
+		</>
+	);
 };
 
 export default Gridcompo;
