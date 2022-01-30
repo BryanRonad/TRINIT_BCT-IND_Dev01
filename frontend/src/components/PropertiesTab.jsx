e0a6f574d3fc26a54c83e0b283711f1037da9c19
BryanRonad <bryanronad@gmail.com>
2022-01-30 11:06:37 +0530

    Grid spanning and properties

--- frontend/src/components/PropertiesTab.jsx
+++ frontend/src/components/PropertiesTab.jsx
@@ -1,34 +1,13 @@
-import {
-  Box,
-  FormControl,
-  FormLabel,
-  Input,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Box, FormControl, FormLabel, Input } from "@chakra-ui/react";
 import React from "react";
 
 const PropertiesTab = (props) => {
   return (
     <Box className="properties-tab">
       This is properties
-	  {console.log(props)}
-      {props.properties && Object.keys(props.properties).map(function (key, index) {
-        if (props.properties instanceof Array) {
-          Object.keys(props.properties[key]).map(function (key1, index1) {
-            return (
-              <FormControl key={index+index1}>
-                <FormLabel htmlFor={key1}>{key1}</FormLabel>
-                <Input
-                  id={key1}
-                  type="text"
-                  maxW="xs"
-                  value={props.properties[key][key1]}
-                  onChange={props.changeprop}
-                />
-              </FormControl>
-            )
-          })
-        } else {
+      {console.log(props)}
+      {props.properties &&
+        Object.keys(props.properties).map(function (key, index) {
           return (
             <FormControl key={index}>
               <FormLabel htmlFor={key}>{key}</FormLabel>
@@ -40,9 +19,8 @@ const PropertiesTab = (props) => {
                 onChange={props.changeprop}
               />
             </FormControl>
-          )
-        }
-      })}
+          );
+        })}
     </Box>
   );
 };
--- frontend/src/elements/Gridcompo.jsx
+++ frontend/src/elements/Gridcompo.jsx
@@ -26,18 +26,21 @@ const Gridcompo = (props) => {
     {
       props.properties.columns !== "" &&
         props.properties.rows !== "" &&
-        [...Array(parseInt(props.properties.rows * props.properties.columns))].map((x, i1) =>
+        [
+          ...Array(parseInt(props.properties.rows * props.properties.columns)),
+        ].map(
+          (x, i1) =>
             temp[props.id]["children"].push({
               elem_id: `${props.id}_${i1}`,
-              gridcolumn: `1 / span 2`,
-              gridrow: `1 / span 2`,
+              gridcolumn: "",
+              gridrow: "",
             }),
           (temp = JSON.parse(JSON.stringify(temp))),
           setwebTags(temp)
         );
     }
 
-    i = 0
+    i = 0;
   };
 
   useEffect(() => {
@@ -57,12 +60,16 @@ const Gridcompo = (props) => {
               key={ele.elem_id}
               id={ele.elem_id}
               onClick={props.selectTag}
-              style={{ width: "auto", minHeight: "50px", background: "red" }}
+              style={{
+                width: "auto",
+                minHeight: "50px",
+                background: "red",
+                gridColumn: `${ele.gridcolumn}`,
+                gridRow: `${ele.gridrow}`,
+              }}
             ></div>
-          )
-        }
-        )
-        }
+          );
+        })}
       </div>
     </>
   );
